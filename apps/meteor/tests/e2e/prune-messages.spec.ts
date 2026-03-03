15c61b17486b0da375487acca242b20b91e4f734
Matheus Cardoso <matheus@cardo.so>
2026-03-03 11:34:32 -0300

    Revert "chore: improve prune messages test"
    This reverts commit 59da1cf0338a9276056cabb9dd04e361fe1fecf6.

--- apps/meteor/tests/e2e/prune-messages.spec.ts
+++ apps/meteor/tests/e2e/prune-messages.spec.ts
@@ -124,7 +124,9 @@ test.describe('prune-messages', () => {
 
 			await test.step('check message list for prune message-attachment', async () => {
 				await expect(content.getLastMessageByFileName('any_file.txt')).not.toBeVisible();
-				await expect(content.lastUserMessage, 'Prune message attachment replaces file attachment').toContainText('File removed by prune');
+				await expect(content.lastMessageTextAttachment, 'Prune message attachment replaces file attachment').toHaveText(
+					'File removed by prune',
+				);
 			});
 		},
 	);
@@ -159,7 +161,7 @@ test.describe('prune-messages', () => {
 
 			await test.step('check main thread message for prune message-attachment', async () => {
 				await expect(content.getLastThreadMessageByFileName('any_file.txt')).not.toBeVisible();
-				await expect(content.lastThreadMessageText, 'Prune message attachment replaces file attachment').toContainText(
+				await expect(content.lastThreadMessageTextAttachment, 'Prune message attachment replaces file attachment').toHaveText(
 					'File removed by prune',
 				);
 			});
